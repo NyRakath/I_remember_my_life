7c2790f7b190293556f53ed21ddb580a3c2e0ecc
NyRakath <soncor23@gmail.com>
2022-06-10 07:33:57 -0700

    Second Change

---
+++ C-Exercises_own/Loops/print_Alpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - mmm
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i == 101)
+		    i += 1;
+		if (i == 113)
+            i += 1;
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
